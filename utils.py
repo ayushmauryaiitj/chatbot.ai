55973ce66fd5d0e5d10ea56355a72660d1354afc
Ayush Maurya <2069ayushmaurya@gmail.com>
2026-03-29 11:13:51 +0530

    Implement text translation using googletrans

--- utils.py
+++ utils.py
@@ -1,2 +1,8 @@
+from googletrans import Translator
+
+translator = Translator()
+
 def translate_text(text, lang):
-    return text
+    if lang == "English":
+        return text
+    return translator.translate(text, dest="hi").text
